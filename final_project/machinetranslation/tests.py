5b1205766c8104f25ee92dd3faad2202ac79abf9
Martin <martingarcia78@hotmail.com>
2023-07-08 17:36:26 -0400

    commit 3

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,22 +1,22 @@
 import unittest
 
-from translator.py import englishToFrench, frenchToEnglish
+from translator import english_to_french, french_to_english
 
 class TestMyModule(unittest.TestCase):
     
     #Tests for english to french
     def test_e2fe(self):
-        self.assertEqual(englishToFrench("Hello"),"Bonjour")
+        self.assertEqual(english_to_french("Hello"),"Bonjour")
 
     def test_e2fne(self):
-        self.assertNotEqual(englishToFrench("cat"),"chien")
+        self.assertNotEqual(english_to_french("cat"),"chien")
 
     #Tests for french to english
     def test_f2e(self):
-        self.assertEqual(frenchToEnglish("Bonjour"), "Hello")
+        self.assertEqual(french_to_english("Bonjour"),"Hello")
     
     def test_f2en(self):
-        self.assertNotEqual(frenchToEnglish("chien"),"cat")
+        self.assertNotEqual(french_to_english("chien"),"cat")
 
 
 if __name__ == "__main__":
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,11 +1,15 @@
+" This file is for creating translation methods "
+
 from deep_translator import MyMemoryTranslator
 
 #English to french function
-def englishToFrench(englishText):
-    frenchText = MyMemoryTranslator(source="english", target="french").translate(englishText) #creating an instance of the class
-    return frenchText
+def english_to_french(english_text):
+    french_text = MyMemoryTranslator(source="english",
+    target="french").translate(english_text) #creating an instance of the class
+    return french_text
 
 #French to english function
-def frenchToEnglish(frenchText):
-    englishText = MyMemoryTranslator(source="french", target="english").translate(frenchText)
-    return englishText
+def french_to_english(french_text):
+    english_text = MyMemoryTranslator(source="french",
+    target="english").translate(french_text)
+    return english_text
